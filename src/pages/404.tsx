805955b310dbae7d5cabb2690e99012571bcb3eb
William Daghouz <w.daghouz@gmail.com>
2021-07-19 23:30:48 +0200

    Removed unused imports from 404 page.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,9 +2,6 @@ import React, { FC, Fragment } from 'react'
 import { PageProps } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import Container from '@material-ui/core/Container'
-import Box from '@material-ui/core/Box'
-import Illustration404 from '../components/illustrations/404illustration'
 import Masthead from '../components/404Page/masthead'
 
 
