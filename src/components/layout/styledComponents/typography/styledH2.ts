7698e84970797751397380f7dfd42cca7d9bfd45
William Daghouz <w.daghouz@gmail.com>
2021-10-30 04:06:05 +0200

    Fixed typo.

--- src/components/layout/styledComponents/typography/styledH2.ts
+++ src/components/layout/styledComponents/typography/styledH2.ts
@@ -2,11 +2,11 @@ import styled from 'styled-components'
 
 import THEME from '../../../../styles/theme'
 
-const StyledH1 = styled.h2`
+const StyledH2 = styled.h2`
   text-align: center;
   font-size: calc(1.6rem + 2vw);
   font-weight: 800;
   margin-bottom: ${THEME.spacing(4)};
 `
 
-export default StyledH1
+export default StyledH2
