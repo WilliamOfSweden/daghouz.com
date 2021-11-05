4d4e1b67222c5032bda533255a8a04c911e71baf
William Daghouz <w.daghouz@gmail.com>
2021-11-05 14:18:54 +0100

    Fixed issue with import of component.

---
+++ src/components/projectsPage/masthead/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
+
+import { DefaultSectionData } from '../../../typescript'
+import FlexSection from '../../common/flexSection'
+
+interface Props {
+  mastheadData: DefaultSectionData
+}
+
+const Masthead = ({ mastheadData }: Props) => (
+  <FlexSection sectionContent={mastheadData}>
+    <StaticImage
+      src='../../../images/projects.png'
+      alt='Isometric web development items and rocket.'
+      loading='eager'
+      formats={['auto', 'webp', 'avif']}
+      placeholder='none'
+      layout='fullWidth'
+    />
+  </FlexSection>
+)
+
+export default Masthead
