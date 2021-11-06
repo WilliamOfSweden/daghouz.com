ab5cf138e15d95271fe9a06a2a2c4ab55edbd0c5
William Daghouz <w.daghouz@gmail.com>
2021-11-06 12:53:32 +0100

    Removed fonts.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,1 @@
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/mulish/900.css'
-import '@fontsource/mulish/800.css'
-import '@fontsource/mulish/700.css'
-
 import './src/styles/global.css'
