c197e3213500ab03914e8b7b8615deb6f2718c91
William Daghouz <w.daghouz@gmail.com>
2022-02-02 08:34:48 +0100

    Modified fonts.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
+import './src/styles/fonts/mulish900.css'
+import './src/styles/fonts/mulish800.css'
+import './src/styles/fonts/mulish700.css'
+import './src/styles/fonts/roboto400.css'
+import './src/styles/fonts/roboto500.css'
+import './src/styles/fonts/roboto700.css'
 import './src/styles/global.css'
