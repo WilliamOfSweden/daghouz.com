9bb3b19e05ef9c92cc7aab4e46e28156a81bef9b
William Daghouz <w.daghouz@gmail.com>
2022-02-20 23:42:22 +0100

    Deleted erroneous import.

--- src/components/indexPageComponents/index.ts
+++ src/components/indexPageComponents/index.ts
@@ -1,13 +1,6 @@
 import AboutSection from './aboutSection/'
-import ECommerceSection from './eCommerceSection/'
 import FeaturesSection from './featuresSection/'
 import JamstackSection from './jamstackSection/'
 import Masthead from './masthead/'
 
-export {
-  AboutSection,
-  ECommerceSection,
-  FeaturesSection,
-  JamstackSection,
-  Masthead,
-}
+export { AboutSection, FeaturesSection, JamstackSection, Masthead }
