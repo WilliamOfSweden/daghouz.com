5ce9e2af4d42f3d6afb5083498907640cab1f0ee
William Daghouz <w.daghouz@gmail.com>
2022-02-25 22:26:53 +0100

    Deleted redundant file.

--- src/utils/mount-canvas.util.ts
+++
@@ -1,9 +0,0 @@
-interface Net extends NetworkInformation {
-  saveData: boolean
-}
-
-const MountCanvas = () =>
-  (typeof window !== 'undefined' && (navigator.connection as Net).saveData) ||
-  !matchMedia('(min-width: 768px)').matches
-
-export default MountCanvas
