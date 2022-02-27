bc1407b87ab6f997aef796dd89b34c9efd81c871
William Daghouz <w.daghouz@gmail.com>
2022-02-27 21:49:50 +0100

    Deleted uncorrect important.

--- src/components/indexPageComponents/index.ts
+++ src/components/indexPageComponents/index.ts
@@ -1,6 +1,5 @@
-import AboutSection from './aboutSection/'
 import FeaturesSection from './featuresSection/'
 import JamstackSection from './jamstackSection/'
 import Masthead from './masthead/'
 
-export { AboutSection, FeaturesSection, JamstackSection, Masthead }
+export { FeaturesSection, JamstackSection, Masthead }
