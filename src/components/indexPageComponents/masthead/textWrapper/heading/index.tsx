9a39adf9e37c175614c71a27f2a42e7b751a3dba
William Daghouz <w.daghouz@gmail.com>
2022-03-11 20:38:12 +0100

    Modified h1 in index page.

--- src/components/indexPageComponents/masthead/textWrapper/heading/index.tsx
+++ src/components/indexPageComponents/masthead/textWrapper/heading/index.tsx
@@ -8,8 +8,19 @@ interface Props {
 }
 
 const Heading = ({ coloredTitle, title }: Props) => (
-  <h1 className={headingStyles.h1}>
-    <span className={headingStyles.colored}>{coloredTitle}</span> {title}
+  <h1>
+    <span className={headingStyles.visuallyHidden}>
+      {coloredTitle} {title}
+    </span>
+    <svg
+      aria-hidden='true'
+      viewBox='0 0 975.8027 90'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <text x={-5} y={73} className={headingStyles.heading}>
+        <tspan className={headingStyles.colored}>{coloredTitle}</tspan> {title}
+      </text>
+    </svg>
   </h1>
 )
 
