af49abf535f8d328b3c8911be0b7e5e761b33bac
William Daghouz <w.daghouz@gmail.com>
2023-09-12 00:38:59 +0200

    feat(Button): create button component including styles and story

---
+++ src/components/atoms/button.stories.ts
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Button } from './'
+import styles from './button.module.css'
+
+const meta: Meta = {
+  component: Button,
+  parameters: {
+    controls: {
+      exclude: ['className'],
+    },
+  },
+  title: 'Atoms/Button',
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    buttonText: 'Contact Me',
+    className: styles.btn,
+    disabled: false,
+  },
+}
