7228f42e5434bf378d03e4e3f4e8541fd5934d4b
William Daghouz <w.daghouz@gmail.com>
2023-09-12 13:56:40 +0200

    chore(deps): install and configure contentful and gatsby dependencies

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,33 @@
+import dotenv from 'dotenv'
 import { GatsbyConfig } from 'gatsby'
 
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 const gatsbyConfig: GatsbyConfig = {
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_API_TOKEN,
+      },
+    },
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          backgroundColor: `transparent`,
+          formats: [`auto`, `webp`, `avif`],
+          placeholder: `none`,
+        },
+      },
+    },
+    'gatsby-transformer-inline-svg',
+    `gatsby-transformer-sharp`,
+  ],
 }
 
 export default gatsbyConfig
